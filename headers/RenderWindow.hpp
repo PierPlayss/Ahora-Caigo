c86426c7e917b57369f3a9cd03d71a1a7ba47467
PierPlayss <37775910+PierPlayss@users.noreply.github.com>
2023-12-05 15:34:48 -0300

    Add files via upload

--- headers/RenderWindow.hpp
+++ headers/RenderWindow.hpp
@@ -15,30 +15,31 @@ class RenderWindow
 public:
 	RenderWindow(const char* p_title, int p_w, int p_h);
 	SDL_Texture* loadTexture(const char* p_filePath);
-	SDL_Texture* Points(const char* msg, int x, int y, int r, int g, int b, int size,int& w, int& h);
+	SDL_Texture* Points(const char* msg, int x, int y, int r, int g, int b, int size, int& w, int& h);
 	void cleanUp();
 	void clear();
 	void render(Entity& p_entity, double angle, float multiplicador);
-	void render(Entity& p_entity,float multiplicador);
-	void renderCenter(Entity& p_entity, float multiplicador,int xOff, int yOff);
+	void render(Entity& p_entity, float multiplicador);
+	void renderCenter(Entity& p_entity, float multiplicador, int xOff, int yOff);
 	void renderAlignTo(Entity& p_entity, Entity& p_entity2, float multiplicador, int alignX, int alignY, int xoff, int yoff);
 	void render(Entity& p_entity, SDL_Rect& rect);
 	void renderFlip(Entity& p_entity, double angle, float multiplicador, bool flip);
 	void display();
-	void animation(Entity& p_entity, SDL_Rect &rect, SDL_Rect& pos);
+	void animation(Entity& p_entity, SDL_Rect& rect, SDL_Rect& pos);
 	void drawText(const char* msg, int x, int y, int r, int g, int b, int a, int size);
-	void textCustom(const char* msg,std::string path, int x, int y, int r, int g, int b, int a, int size,bool center, int xoff, int yoff, int quality);
+	void textCustom(const char* msg, std::string path, int x, int y, int r, int g, int b, int a, int size, bool center, int xoff, int yoff, int quality);
 	void textAlignTo(const char* msg, Entity& p_entity, int r, int g, int b, int a, int size, int alignX, int alignY, int xoff, int yoff, std::string path);
-	void backgroundColor(int r, int g, int b,int a);
-	void rectangle(int x, int y, int w, int h, int r, int g, int b, int a,bool filled);
+	void backgroundColor(int r, int g, int b, int a);
+	void rectangle(int x, int y, int w, int h, int r, int g, int b, int a, bool filled);
+	float easyInOut(int startPos, int endPos, float duration, int &currentStep);
 	void ToggleFullscreen();
-	
+
 	/*static SDL_Texture* loadFont(const std::string& font_path, int font_size, const std::string& message_text, const SDL_Color& color);*/
 
-	
+
 private:
 	SDL_Renderer* renderer;
-	SDL_Window *window = nullptr;
-	
+	SDL_Window* window = nullptr;
+
 
 };
