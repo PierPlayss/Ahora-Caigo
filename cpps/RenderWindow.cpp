2ad8a04eee0b10fb2e038a8085b9425474d058ab
PierPlayss <37775910+PierPlayss@users.noreply.github.com>
2023-12-05 15:34:34 -0300

    Add files via upload

--- cpps/RenderWindow.cpp
+++ cpps/RenderWindow.cpp
@@ -48,7 +48,7 @@ void RenderWindow::drawText(const char* msg, int x, int y, int r, int g, int b, 
 	SDL_Surface* surf;
 	SDL_Texture* texture;
 	TTF_Font* font = TTF_OpenFont("res/fonts/kanit.otf", size);
-	
+
 	SDL_Color color;
 	color.r = r;
 	color.g = g;
@@ -56,26 +56,26 @@ void RenderWindow::drawText(const char* msg, int x, int y, int r, int g, int b, 
 	color.a = 255;
 	SDL_Rect rect;
 	surf = TTF_RenderText_Blended(font, msg, color);
-	
+
 	texture = SDL_CreateTextureFromSurface(renderer, surf);
 	SDL_SetTextureAlphaMod(texture, a);
 	rect.x = x;
 	rect.y = y;
-	rect.w = surf -> w  ;
-	rect.h = surf -> h  ;
-	TTF_CloseFont(font); 
+	rect.w = surf->w;
+	rect.h = surf->h;
+	TTF_CloseFont(font);
 	//IMG_SavePNG(surf, "image.png");
 	//SDL_SaveBMP(surf, "image.bmp");
 	SDL_FreeSurface(surf);
-	
-	
 
-	SDL_RenderCopy(renderer,texture,NULL,&rect);
+
+
+	SDL_RenderCopy(renderer, texture, NULL, &rect);
 	SDL_DestroyTexture(texture);
 
 }
 
-void RenderWindow::textCustom(const char* msg, std::string path, int x, int y, int r, int g, int b, int a, int size, bool center,int xoff,int yoff,int quality) {
+void RenderWindow::textCustom(const char* msg, std::string path, int x, int y, int r, int g, int b, int a, int size, bool center, int xoff, int yoff, int quality) {
 	int width = 1280;
 	int height = 720;
 	SDL_Surface* surf;
@@ -95,7 +95,7 @@ void RenderWindow::textCustom(const char* msg, std::string path, int x, int y, i
 	switch (quality) {
 	case 1: surf = TTF_RenderText_Blended(font, msg, color);
 		break;
-	case 2: surf = TTF_RenderText_Shaded(font, msg, color,color);
+	case 2: surf = TTF_RenderText_Shaded(font, msg, color, color);
 		break;
 	case 0: surf = TTF_RenderText_Solid(font, msg, color);
 		break;
@@ -103,15 +103,15 @@ void RenderWindow::textCustom(const char* msg, std::string path, int x, int y, i
 		surf = TTF_RenderText_Solid(font, msg, color);
 
 	}
-	
 
-	
+
+
 	texture = SDL_CreateTextureFromSurface(renderer, surf);
 	SDL_SetTextureAlphaMod(texture, a);
 
 	if (center == true) {
-		rect.x = width/2- ((surf->w)/2)+xoff;
-		rect.y = height / 2 - ((surf->h)/2)+yoff;
+		rect.x = width / 2 - ((surf->w) / 2) + xoff;
+		rect.y = height / 2 - ((surf->h) / 2) + yoff;
 		rect.w = surf->w;
 		rect.h = surf->h;
 	}
@@ -144,8 +144,8 @@ void RenderWindow::textAlignTo(const char* msg, Entity& p_entity, int r, int g, 
 	SDL_Texture* texture;
 
 	TTF_Font* font = TTF_OpenFont(path.c_str(), size);
-	
-	
+
+
 
 	SDL_Color color;
 	color.r = r;
@@ -160,7 +160,7 @@ void RenderWindow::textAlignTo(const char* msg, Entity& p_entity, int r, int g, 
 	//0= no align 1=center 2=right
 
 
-	switch (alignX){
+	switch (alignX) {
 	case 0:
 		totalX += p_entity.getCurrentFrame().x;
 		break;
@@ -189,15 +189,15 @@ void RenderWindow::textAlignTo(const char* msg, Entity& p_entity, int r, int g, 
 		totalY += p_entity.getCurrentFrame().y;
 		break;
 	}
-	
 
 
-	rect.x = totalX +xoff;
-	rect.y = totalY +yoff;
+
+	rect.x = totalX + xoff;
+	rect.y = totalY + yoff;
 	rect.w = surf->w;
 	rect.h = surf->h;
 
-	
+
 
 	TTF_CloseFont(font);
 	//IMG_SavePNG(surf, "image.png");
@@ -239,7 +239,7 @@ void RenderWindow::render(Entity& p_entity, double angle, float multiplicador) {
 	dst.w = p_entity.getCurrentFrame().w * multiplicador;
 	dst.h = p_entity.getCurrentFrame().h * multiplicador;
 
-	SDL_RenderCopyEx(renderer, p_entity.getTex(), &src, &dst,angle,NULL, SDL_FLIP_NONE);
+	SDL_RenderCopyEx(renderer, p_entity.getTex(), &src, &dst, angle, NULL, SDL_FLIP_NONE);
 }
 
 void RenderWindow::renderFlip(Entity& p_entity, double angle, float multiplicador, bool flip) {
@@ -262,7 +262,7 @@ void RenderWindow::renderFlip(Entity& p_entity, double angle, float multiplicado
 
 	if (flip == 1) {
 		flipType = SDL_FLIP_HORIZONTAL;
-	
+
 	}
 	else flipType = SDL_FLIP_NONE;
 
@@ -282,12 +282,12 @@ void RenderWindow::render(Entity& p_entity, float multiplicador)
 	dst.x = p_entity.getX() * multiplicador;
 	dst.y = p_entity.getY() * multiplicador;
 	dst.w = p_entity.getCurrentFrame().w * multiplicador;
-	dst.h = p_entity.getCurrentFrame().h *multiplicador;
+	dst.h = p_entity.getCurrentFrame().h * multiplicador;
 
 	SDL_RenderCopy(renderer, p_entity.getTex(), &src, &dst);
 }
 void RenderWindow::renderCenter(Entity& p_entity, float multiplicador, int xOff, int yOff)
-{	
+{
 	int width = 1280;
 	int height = 720;
 
@@ -298,8 +298,8 @@ void RenderWindow::renderCenter(Entity& p_entity, float multiplicador, int xOff,
 	src.h = p_entity.getCurrentFrame().h;
 
 	SDL_Rect dst;
-	dst.x = width/2 - ((p_entity.getCurrentFrame().w * multiplicador)/2)+xOff;
-	dst.y = height/2 -((p_entity.getCurrentFrame().h * multiplicador)/2)+yOff;
+	dst.x = width / 2 - ((p_entity.getCurrentFrame().w * multiplicador) / 2) + xOff;
+	dst.y = height / 2 - ((p_entity.getCurrentFrame().h * multiplicador) / 2) + yOff;
 	dst.w = p_entity.getCurrentFrame().w * multiplicador;
 	dst.h = p_entity.getCurrentFrame().h * multiplicador;
 
@@ -307,7 +307,7 @@ void RenderWindow::renderCenter(Entity& p_entity, float multiplicador, int xOff,
 }
 
 void RenderWindow::renderAlignTo(Entity& p_entity, Entity& p_entity2, float multiplicador, int alignX, int alignY, int xoff, int yoff) {
-	
+
 	int totalX = 0, totalY = 0;
 
 
@@ -317,8 +317,8 @@ void RenderWindow::renderAlignTo(Entity& p_entity, Entity& p_entity2, float mult
 	src.w = p_entity.getCurrentFrame().w;
 	src.h = p_entity.getCurrentFrame().h;
 
-	
-	
+
+
 	switch (alignX) {
 	case 0:
 		totalX = p_entity2.getCurrentFrame().x;
@@ -328,7 +328,7 @@ void RenderWindow::renderAlignTo(Entity& p_entity, Entity& p_entity2, float mult
 		break;
 	case 2:
 		totalX = p_entity2.getCurrentFrame().w - (p_entity.getCurrentFrame().w);
-	
+
 		break;
 	default:
 		totalX = p_entity2.getCurrentFrame().x;
@@ -352,10 +352,10 @@ void RenderWindow::renderAlignTo(Entity& p_entity, Entity& p_entity2, float mult
 
 
 	SDL_Rect dst;
-	
 
-	dst.x = totalX+xoff;
-	dst.y = totalY+yoff;
+
+	dst.x = totalX + xoff;
+	dst.y = totalY + yoff;
 	dst.w = p_entity.getCurrentFrame().w * multiplicador;
 	dst.h = p_entity.getCurrentFrame().h * multiplicador;
 
@@ -363,7 +363,7 @@ void RenderWindow::renderAlignTo(Entity& p_entity, Entity& p_entity2, float mult
 
 }
 
-void RenderWindow::render(Entity& p_entity, SDL_Rect &rect)
+void RenderWindow::render(Entity& p_entity, SDL_Rect& rect)
 {
 	SDL_Rect src;
 	src.x = rect.x;
@@ -375,14 +375,14 @@ void RenderWindow::render(Entity& p_entity, SDL_Rect &rect)
 	dst.x = p_entity.getX();
 	dst.y = p_entity.getY();
 	dst.w = p_entity.getCurrentFrame().w;
-	dst.h = p_entity.getCurrentFrame().h/4;
+	dst.h = p_entity.getCurrentFrame().h / 4;
 
 	SDL_RenderCopy(renderer, p_entity.getTex(), &src, &dst);
 }
 
 
-void RenderWindow::animation(Entity& p_entity,SDL_Rect &rect, SDL_Rect& pos) {
-	
+void RenderWindow::animation(Entity& p_entity, SDL_Rect& rect, SDL_Rect& pos) {
+
 	SDL_Rect dst;
 	dst.x = rect.x;
 	dst.y = rect.y;
@@ -399,7 +399,7 @@ void RenderWindow::animation(Entity& p_entity,SDL_Rect &rect, SDL_Rect& pos) {
 	SDL_RenderCopy(renderer, p_entity.getTex(), &dst, &final);
 }
 
-void RenderWindow::backgroundColor(int r, int g, int b,int a) {
+void RenderWindow::backgroundColor(int r, int g, int b, int a) {
 	SDL_SetRenderDrawColor(renderer, r, g, b, a);
 }
 
@@ -415,7 +415,7 @@ void RenderWindow::ToggleFullscreen() {
 	SDL_ShowCursor(IsFullscreen);
 }
 
-void RenderWindow::rectangle(int x, int y,int w,int h, int r, int g,int b,int a,bool filled) {
+void RenderWindow::rectangle(int x, int y, int w, int h, int r, int g, int b, int a, bool filled) {
 	//SDL_RenderClear(renderer);
 
 	SDL_Rect rect;
@@ -426,7 +426,7 @@ void RenderWindow::rectangle(int x, int y,int w,int h, int r, int g,int b,int a,
 
 	SDL_SetRenderDrawColor(renderer, r, g, b, a);
 	if (filled == true) {
-		
+
 		SDL_RenderFillRect(renderer, &rect);
 
 	}
@@ -434,12 +434,45 @@ void RenderWindow::rectangle(int x, int y,int w,int h, int r, int g,int b,int a,
 		SDL_RenderDrawRect(renderer, &rect);
 	}
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, a);
-		SDL_RenderPresent(renderer);
-	
-	
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, a);
+	SDL_RenderPresent(renderer);
+
+
 }
+float RenderWindow::easyInOut(int startPos, int endPos, float duration, int &currentStep) {
+
+	//float startPos = -90, endPos = 200;
+	//float currentPos = startPos;
+	const int FPS = 60;
+	float change;
+	float t = 0;
+	//float duration = 1;
+	//int currentStep = 0;
+	int totalSteps = duration * FPS;
 
 
+	if (currentStep < totalSteps) {
+
+		t = (currentStep * 1.0) / totalSteps;
+
+		if (t <= 0.5) {
+			change = 2.0 * t * t;
+			change = startPos + (change * (endPos - startPos)) / 1;
+
+		}
+		else {
+			t -= 0.5;
+			change = 2.0 * t * (1.0 - t) + 0.5;
+			change = startPos + (change * (endPos - startPos)) / 1;
+
+		}
+
+
+
+		currentStep++;
+		return change;
+	}
+
+}
 
 
